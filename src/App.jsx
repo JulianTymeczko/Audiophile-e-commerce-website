b4024e386d08de98802fe4ca8cfa485fa9b8fd65
Julian Houle-T <jhouletymeczko@gmail.com>
2023-10-26 02:14:28 -0400

    working on Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import "./App.css";
 import Nav from "./Nav";
 import Footer from "./Footer";
+import Home from "./pages/Home";
+import Checkout from "./pages/Checkout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <Nav></Nav>
+      <Router>
+        <Nav></Nav>
+        <Routes>
+          <Route>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Home />
+                </>
+              }
+            />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,12 +3,32 @@ import Button from "./components/buttons/Button";
 import TextForm from "./components/forms/TextForm";
 import RadioForm from "./components/forms/RadioForm";
 import CounterForm from "./components/forms/CounterForm";
+import { Link } from "react-router-dom";
 function Nav() {
   const onClickFunc = () => {
     alert("hey there");
   };
   return (
     <>
+      <nav>
+        <button type="button" className="hamburger-button">
+          <img src="/assets/shared/tablet/icon-hamburger.svg" alt="" />
+        </button>
+        <Link to="/" className="logo">
+          <img src="/assets/shared/desktop/logo.svg" alt="" />
+        </Link>
+
+        <div className="nav-middle">
+          <Link to="/">HOME</Link>
+          <Link to="/headphones">HEADPHONES</Link>
+          <Link to="/speakers">SPEAKERS</Link>
+          <Link to="/earphones">EARPHONES</Link>
+        </div>
+        <button type="button" className="cart-button">
+          <img src="/assets/shared/desktop/icon-cart.svg" alt="" />
+        </button>
+      </nav>
+
       <Button
         buttonText="SEE PRODUCT"
         buttonNumber="one"
